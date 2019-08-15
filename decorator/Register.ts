deca13e787525a7417329ed010b9aaa6b3af7c9d
曾藤辉 <13990290259>
2019-08-15 14:38:18 +0800

    修改register

--- decorator/Register.ts
+++ decorator/Register.ts
@@ -1,11 +1,11 @@
 import Storage from '../index'
 
 export default function register(options: { type: string, route?: string }): Function {
-    return (target: Object, method: string) => {
+    return (target: Object, method: string, descriptor: PropertyDescriptor) => {
         Storage.Actions.push({
             type: options.type,
             method,
-            route: (options && options.route) || method,
+            route: options.route || method,
             target: target.constructor,
             controller: target.constructor.name
         })
